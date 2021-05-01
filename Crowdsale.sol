35ca0c903202444d4c0675a94565387715951a75
Blake Cizek <b.cizek@f5.com>
2021-04-30 18:15:21 -0700

    push

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -8,12 +8,19 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 // @TODO: Inherit the crowdsale contracts
-contract PupperCoinSale is {
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale  {
+
+    uint fakenow = now;
 
     constructor(
         // @TODO: Fill in the constructor parameters!
+        uint rate, address payable wallet, PupperCoin token, uint goal, uint open, uint close
     )
         // @TODO: Pass the constructor parameters to the crowdsale contracts.
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(goal)
+        TimedCrowdsale(now, now + 24 weeks)
+        RefundableCrowdsale(goal)
         public
     {
         // constructor can stay empty
@@ -27,14 +34,22 @@ contract PupperCoinSaleDeployer {
 
     constructor(
         // @TODO: Fill in the constructor parameters!
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint goal
     )
         public
     {
         // @TODO: create the PupperCoin and keep its address handy
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        token_address = address(token);
 
         // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-
+        PupperCoinSale pupper_token = new PupperCoinSale(1, wallet, token, goal, now, now + 24 weeks);
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
+        token_sale_address = address(pupper_token);
+        
         token.addMinter(token_sale_address);
         token.renounceMinter();
     }
